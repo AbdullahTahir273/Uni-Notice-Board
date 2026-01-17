df3253e254ca443c8e71222631b0f74f853c6c97
umairakhtar0 <trdumair@gmail.com>
2026-01-17 18:39:57 +0500

    Update and rename .stylelintrc.json to stylelint.config.js

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: 'stylelint-config-standard',
+  rules: {
+    indentation: 2,
+    'string-quotes': 'double',
+    'color-hex-case': 'lower',
+    'max-empty-lines': 2
+  }
+};
